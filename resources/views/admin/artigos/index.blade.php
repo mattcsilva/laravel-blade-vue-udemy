19fba558fbe5e2d8458764ed6eb6513b46247c7d
matheuscabral <matheus.cabral@connectparts.com.br>
2020-07-03 12:01:59 -0300

    Adicionado mais funcionalidades

---
+++ resources/views/admin/artigos/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <pagina tamanho="12">
+        <painel titulo="Lista de Artigos">
+            <tabela-lista 
+            v-bind:titulos="['#', 'Título', 'Curso']"
+            v-bind:itens="[[1, 'PHP 00', 'Curso de PHP 00'],[2, 'Vue JS', 'Curso de Vue JS']]"
+            criar="#criar"
+            detalhe="#detalhe"
+            editar="#editar"
+            deletar="#deletar"
+            token="666666666"
+            
+            ></tabela-lista>
+
+        </painel>
+    </pagina>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,14 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+/**
+ * prefix (localhost:8000/admin/artigos)
+ * namespace (foi criado o namespace admin e agora será usado)
+ * middleware é passado como padrão para não adicionar em todos os construtores
+ */
+Route::middleware(['auth'])->prefix('admin')->namespace('Admin')->group(function(){
+
+    Route::resource('artigos', 'ArtigosController');
+
+});
